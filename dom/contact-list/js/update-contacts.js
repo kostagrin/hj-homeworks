c3a4c9541e6e251ef96addf394d4f7308a4ea2ce
kostagrin <konstantingrinev7@gmail.com>
2019-01-02 19:08:53 +0300

    try/catch question

--- dom/contact-list/js/update-contacts.js
+++ dom/contact-list/js/update-contacts.js
@@ -1,15 +1,32 @@
 'use strict';
 const contacts = JSON.parse(loadContacts());
-const [contactList] = document.getElementsByClassName('contacts-list');
+
+// try {
+// 	const contacts = JSON.parse(loadContacts());
+// } catch (err) {
+// 	return err;
+// }
+
+
+// const contacts = {};
+// try {
+// contacts = JSON.parse(loadContacts());
+// } catch (err) {
+// 	return err;
+// }
+
+
+const contactList = document.querySelector('.contacts-list');
 
 (function fillInContactList() {
 	for (let i = 0; i < contacts.length; i++) {
 		let li = document.createElement('LI');
+		let strong = document.createElement('STRONG');
 
-		contactList.appendChild( li );
-		contactList.getElementsByTagName('LI')[i].appendChild( document.createElement('STRONG') );
+		strong.innerHTML = contacts[i].name;
+		contactList.appendChild(li);
+		li.appendChild(strong);
 		li.dataset.email = contacts[i].email;
 		li.dataset.phone = contacts[i].phone;
-		contactList.getElementsByTagName('STRONG')[i].innerHTML = `${contacts[i].name}`;
-	}   
+	}
 })();
