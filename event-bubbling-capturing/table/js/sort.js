cbd7d9d688eb3eedd340d6f648dfcb487879c0fd
kostagrin <konstantingrinev7@gmail.com>
2019-01-20 20:07:28 +0300

    На проверку

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,14 @@
 'use strict';
-
 function handleTableClick(event) {
-  
+	if (event.target.tagName === 'TH') {
+		event.target.dataset.dir == 1 ?
+			event.target.dataset.dir = -1 :
+			event.target.dataset.dir = 1;
+
+		event.currentTarget.dataset.sortBy =
+			event.target.dataset.propName;
+
+		sortTable(event.target.dataset.propName,
+			event.target.dataset.dir);
+	}
 }
