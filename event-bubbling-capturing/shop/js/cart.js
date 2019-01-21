30c69604597125bf0c3ad4c01931807a4efb7886
kostagrin <konstantingrinev7@gmail.com>
2019-01-21 18:53:10 +0300

    На проверку

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,7 @@
 'use strict';
+function updateCart(event) {
+	if (!event.target.classList.contains('add-to-cart')) return;
+	event.preventDefault();
+	addToCart(event.target.dataset);
+}
+list.addEventListener('click', updateCart);
