da279759ee002882462745a5797d6c9d89e4139b
kostagrin <konstantingrinev7@gmail.com>
2019-01-23 21:17:32 +0300

    На проверку

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const counterBox = document.getElementById('counter');
+const btns = document.querySelector('.wrap-btns');
+
+btns.addEventListener('click', count);
+
+let counter = 0;
+
+function count(event) {
+	switch (event.target) {
+		case (increment):
+			counter++;
+			break;
+		case (decrement):
+			if (localStorage.getItem('counterValue') == 0) {
+				return;
+			}
+			counter--;
+			break;
+		case (reset):
+			counter = 0;
+			localStorage.clear();
+			location.reload();
+	}
+	saveSession();
+	displaySessionValue();
+}
+
+
+function saveSession() {
+	localStorage.setItem('counterValue', counter);
+}
+
+function displaySessionValue() {
+	counterBox.innerHTML = localStorage.counterValue;
+}
+
+document.addEventListener('DOMContentLoaded', saveSession);
+document.addEventListener('DOMContentLoaded', displaySessionValue);
