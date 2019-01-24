953acd193b67aab50a277620750c8991115bde4e
kostagrin <konstantingrinev7@gmail.com>
2019-01-24 23:11:10 +0300

    Два варианта на проверку

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,67 +1,117 @@
 'use strict';
-const signUpForm = document.querySelector('.sign-up-htm');
-const signUpBtn = signUpForm.querySelector('.button');
-const signUpOutput = signUpForm.querySelector('.error-message');
-
-
-const signInForm = document.querySelector('.sign-in-htm');
-const signInBtn = signInForm.querySelector('.button');
-const signInOutput = signInForm.querySelector('.error-message');
-
+//ПЕРВЫЙ ВАРИАНТ СО ВСПЛЫТИЕМ
 const xhr = new XMLHttpRequest();
+const form = document.getElementsByClassName('login-form')[0];
 
-
-function signUp(event) {
+function logIn(event) {
 	event.preventDefault();
+	if (!event.target.classList.contains('button')) {
+		return;
+	}
 
-	let signUpFormData = new FormData(signUpForm);	
+	let currentForm = event.target.parentElement.parentElement;
+
+	const formData = new FormData(currentForm);
 	let requestObj = {};
+	let path = event.target.parentElement.parentElement
+		.getAttribute('class').split('-').slice(0, 2).join('');
 
-	for (let [key, value] of signUpFormData) {
+	const outputMessage =
+		event.target.parentElement.parentElement
+		.querySelector('.error-message');
+
+	for (let [key, value] of formData) {
 		requestObj[key] = value;
 	}
-		
+
 	xhr.addEventListener('load', onLoad);
-	xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
+	xhr.open('POST', `https://neto-api.herokuapp.com/${path}`);
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.send(JSON.stringify(requestObj));
 
 	function onLoad() {
 		let response = JSON.parse(xhr.responseText);
+
 		if (response.error) {
-			signUpOutput.value = response.message;
+			outputMessage.value = response.message;
 		} else {
-			signUpOutput.value = `Пользователь ${response.name} успешно зарегистрирован`;
+			let word = '';
+			switch (path) {
+				case ('signup'):
+					word = 'зарегистрирован';
+					break;
+				case ('signin'):
+					word = 'авторизован';
+			}
+			outputMessage.value = `Пользователь ${response.name} успешно ${word}`;
 		}
 	}
 }
 
+form.addEventListener('click', logIn);
 
-function signIn(event) {
-	event.preventDefault();
+// ВТОРОЙ ВАРИАНТ С ПОВТОРЕНИЯМИ
+//===============================
+// const signUpForm = document.querySelector('.sign-up-htm');
+// const signUpBtn = signUpForm.querySelector('.button');
+// const signUpOutput = signUpForm.querySelector('.error-message');
 
-	let signInFormData = new FormData(signInForm);	
-	let requestObj = {};
-
-	for (let [key, value] of signInFormData) {
-		requestObj[key] = value;
-	}
-		
-	xhr.addEventListener('load', onLoad);
-	xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify(requestObj));
-
-	function onLoad() {
-		let response = JSON.parse(xhr.responseText);
-		if (response.error) {
-			signInOutput.value = response.message;
-		} else {
-			signInOutput.value = `Пользователь ${response.name} успешно авторизован`;
-		}
-	}
-}
+// const signInForm = document.querySelector('.sign-in-htm');
+// const signInBtn = signInForm.querySelector('.button');
+// const signInOutput = signInForm.querySelector('.error-message');
 
 
-signUpBtn.addEventListener('click', signUp);
-signInBtn.addEventListener('click', signIn);
+// function signUp(event) {
+// 	event.preventDefault();
+
+// 	let signUpFormData = new FormData(signUpForm);	
+// 	let requestObj = {};
+
+// 	for (let [key, value] of signUpFormData) {
+// 		requestObj[key] = value;
+// 	}
+
+// 	xhr.addEventListener('load', onLoad);
+// 	xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
+// 	xhr.setRequestHeader('Content-Type', 'application/json');
+// 	xhr.send(JSON.stringify(requestObj));
+
+// 	function onLoad() {
+// 		let response = JSON.parse(xhr.responseText);
+// 		if (response.error) {
+// 			signUpOutput.value = response.message;
+// 		} else {
+// 			signUpOutput.value = `Пользователь ${response.name} успешно зарегистрирован`;
+// 		}
+// 	}
+// }
+
+
+// function signIn(event) {
+// 	event.preventDefault();
+
+// 	let signInFormData = new FormData(signInForm);	
+// 	let requestObj = {};
+
+// 	for (let [key, value] of signInFormData) {
+// 		requestObj[key] = value;
+// 	}
+
+// 	xhr.addEventListener('load', onLoad);
+// 	xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
+// 	xhr.setRequestHeader('Content-Type', 'application/json');
+// 	xhr.send(JSON.stringify(requestObj));
+
+// 	function onLoad() {
+// 		let response = JSON.parse(xhr.responseText);
+// 		if (response.error) {
+// 			signInOutput.value = response.message;
+// 		} else {
+// 			signInOutput.value = `Пользователь ${response.name} успешно авторизован`;
+// 		}
+// 	}
+// }
+
+
+// signUpBtn.addEventListener('click', signUp);
+// signInBtn.addEventListener('click', signIn);
