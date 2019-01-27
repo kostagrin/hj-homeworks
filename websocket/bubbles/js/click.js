d36f01a48e31fcc77ff033ee669a1f944f1913df
kostagrin <konstantingrinev7@gmail.com>
2019-01-28 00:49:14 +0300

    На проверку

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,15 @@
 'use strict';
+const wsc = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+const canvas = document.querySelector('canvas');
+
+wsc.addEventListener('open', () => {
+	showBubbles(wsc);
+});
+
+
+canvas.addEventListener('click', event => {
+	wsc.send(JSON.stringify({
+		x: event.x,
+		y: event.y
+	}));
+});
