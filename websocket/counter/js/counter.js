24780889f502d1f7b2998e15cb85fa484d9c99d4
kostagrin <konstantingrinev7@gmail.com>
2019-01-28 16:40:12 +0300

    На проверку

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,19 @@
 'use strict';
+const wsc = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('.errors');
+
+wsc.addEventListener('open', () => {
+	wsc.addEventListener('message', showMessage);
+
+});
+
+function showMessage(event) {
+	let message = JSON.parse(event.data);
+	counter.innerHTML = message.connections;
+	errors.value = message.errors;
+}
+
+window.addEventListener('beforeunload', () => {
+	wsc.close(1000);
+});
