f6a94ea8baed33887dfc36162fa8e2d83f90e020
kostagrin <konstantingrinev7@gmail.com>
2019-01-30 21:05:09 +0300

    На проверку

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,24 @@
 'use strict';
+const pic = document.querySelector('[data-pic]');
+const tweets = document.querySelector('[data-tweets]');
+const username = document.querySelector('[data-username]');
+const followers = document.querySelector('[data-followers]');
+const following = document.querySelector('[data-following]');
+const wallpaper = document.querySelector('[data-wallpaper]');
+const description = document.querySelector('[data-description]');
+
+const url = `https://neto-api.herokuapp.com/twitter/jsonp`;
+const script = document.createElement('script');
+script.setAttribute('src', url);
+
+function callback(userData) {
+	pic.src = userData.pic;
+	tweets.value = userData['tweets'];
+	username.innerHTML = userData['username'];
+	followers.value = userData['followers'];
+	following.value = userData['following'];
+	wallpaper.src = userData['wallpaper'];
+	description.innerHTML = userData['description'];
+}
+
+document.body.appendChild(script);
