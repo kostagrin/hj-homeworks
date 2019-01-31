2260c025ad800c6f53ebe2fa7470af8cf6e7164a
kostagrin <konstantingrinev7@gmail.com>
2019-01-31 14:26:50 +0300

    На проверку

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,1 +1,34 @@
 'use strict';
+function loadData(url) {
+	const functionName = 'randName' + Math.floor(Math.random() * 10);
+	return new Promise((resolve, reject) => {
+		window[functionName] = resolve;
+		const script = document.createElement('script');
+		script.src = `${url}?jsonp=${functionName}`;
+		document.body.appendChild(script);
+	});
+}
+
+
+function setUpProfile(data) {
+	document.querySelector('[data-name]').innerHTML = data['name'];
+	document.querySelector('[data-description]').innerHTML = data['description'];
+	document.querySelector('[data-pic]').src = data['pic'];
+	document.querySelector('[data-position]').innerHTML = data['position'];
+
+	loadData(`https://neto-api.herokuapp.com/profile/${data.id}/technologies`)
+		.then(setUpTechnologies);
+		document.querySelector('.content').style = 'display: initial';
+}
+
+function setUpTechnologies(data) {
+	data.forEach(el => {
+		const span = document.createElement('SPAN');
+		span.setAttribute('class', `devicons devicons-${el}`);
+		document.querySelector('[data-technologies]')
+			.appendChild(span);
+	});
+}
+
+loadData('https://neto-api.herokuapp.com/profile/me')
+	.then(setUpProfile);
