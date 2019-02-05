97eddfd69576565b39c5c30f3314bc3907f1405c
kostagrin <konstantingrinev7@gmail.com>
2019-02-05 18:08:36 +0300

    На проверку

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,10 @@
 'use strict';
+const wsc = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+wsc.addEventListener('open', event => {
+	editor.addEventListener('update', updateCanv);
+});
+
+function updateCanv(event) {
+	event.canvas.toBlob(blob => wsc.send(blob));
+}
